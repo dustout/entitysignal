2b7536a8c2a17922a900458fa3e5cc5f9ecc004e
Dustin <dustout@gmail.com>
2019-03-30 13:35:42 -0600

    Making changes to make nginx hosting work

--- EntitySignal/Program.cs
+++ EntitySignal/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace EntitySignal
 {
@@ -19,6 +12,7 @@ namespace EntitySignal
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
+          .UseUrls("http://localhost:5004")
+          .UseStartup<Startup>();
   }
 }
--- EntitySignal/Startup.cs
+++ EntitySignal/Startup.cs
@@ -14,6 +14,7 @@ using EntitySignal.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using EntitySignal.Hubs;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace EntitySignal
 {
@@ -62,6 +63,11 @@ namespace EntitySignal
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
+
+      app.UseForwardedHeaders(new ForwardedHeadersOptions
+      {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+      });
 
       app.UseHttpsRedirection();
       app.UseStaticFiles();
