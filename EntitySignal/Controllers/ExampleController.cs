33be54bb19feea2f03b41c5064c55238f1b4ac65
Dustin <dustout@gmail.com>
2019-04-22 19:52:48 -0600

    Added examples controller

---
+++ EntitySignal/Controllers/ExampleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntitySignal.Controllers
+{
+  public class ExampleController : Controller
+  {
+    public IActionResult AngularJs()
+    {
+      return View();
+    }
+  }
+}
