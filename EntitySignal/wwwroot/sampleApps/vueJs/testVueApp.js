82586448646d73bf28013fc0d07152c983d1110e
Dustin <dustout@gmail.com>
2019-04-24 23:16:20 -0600

    Added vuejs example

--- EntitySignal/wwwroot/sampleApps/vueJs/testVueApp.js
+++ EntitySignal/wwwroot/sampleApps/vueJs/testVueApp.js
@@ -1,18 +1,92 @@
 var client = new EntitySignal.Client();
 client.options.debug = true;
 client.connect();
-var data2 = {
-    list: null,
-    status: client.status
+var data = {
+    messages: null,
+    jokes: null,
+    filterMessages: null,
+    guidJokes: null,
+    maxMessagesCount: 4,
+    maxFilteredMessagesCount: 4,
+    maxJokesCount: 4,
+    maxGuidJokesCount: 4,
+    client: client
 };
-var syncList;
+var methods = {
+    createNew: function () {
+        Vue.http.get("/crud/create");
+    },
+    createFiveNew: function () {
+        Vue.http.get("/crud/createFive");
+    },
+    changeRandom: function () {
+        Vue.http.get("/crud/changeRandom");
+    },
+    deleteAll: function () {
+        Vue.http.get("/crud/deleteAll");
+    },
+    deleteRandom: function () {
+        Vue.http.get("/crud/deleteRandom");
+    }
+};
+var computed = {
+    cMessages: function () {
+        var orderedArray = _.orderBy(data.messages, 'id', 'desc');
+        if (data.maxMessagesCount) {
+            return orderedArray.slice(0, data.maxMessagesCount);
+        }
+        else {
+            return orderedArray;
+        }
+    },
+    cJokes: function () {
+        var orderedArray = _.orderBy(data.jokes, 'id', 'desc');
+        if (data.maxJokesCount) {
+            return orderedArray.slice(0, data.maxJokesCount);
+        }
+        else {
+            return orderedArray;
+        }
+    },
+    cFilterMessages: function () {
+        var orderedArray = _.orderBy(data.filterMessages, 'id', 'desc');
+        if (data.maxFilteredMessagesCount) {
+            return orderedArray.slice(0, data.maxFilteredMessagesCount);
+        }
+        else {
+            return orderedArray;
+        }
+    },
+    cGuidJokes: function () {
+        var orderedArray = _.orderBy(data.guidJokes, 'id', 'desc');
+        if (data.maxGuidJokesCount) {
+            return orderedArray.slice(0, data.maxGuidJokesCount);
+        }
+        else {
+            return orderedArray;
+        }
+    }
+};
 client.syncWith("/subscribe/SubscribeToAllMessages")
     .then(function (x) {
-    data2.list = x;
-    syncList = x;
+    data.messages = x;
+});
+client.syncWith("/subscribe/SubscribeToAllJokes")
+    .then(function (x) {
+    data.jokes = x;
+});
+client.syncWith("/subscribe/SubscribeToOddIdMessages")
+    .then(function (x) {
+    data.filterMessages = x;
+});
+client.syncWith("/subscribe/SubscribeToJokesWithGuidAnswer")
+    .then(function (x) {
+    data.guidJokes = x;
 });
 new Vue({
     el: '#app2',
-    data: data2
+    data: data,
+    methods: methods,
+    computed: computed
 });
 //# sourceMappingURL=testVueApp.js.map
--- EntitySignal/wwwroot/sampleApps/vueJs/testVueApp.ts
+++ EntitySignal/wwwroot/sampleApps/vueJs/testVueApp.ts
@@ -1,43 +1,101 @@
-﻿var client = new EntitySignal.Client();
+﻿declare var Vue: any;
+declare var _: any;
+
+
+var client = new EntitySignal.Client();
 client.options.debug = true;
 client.connect();
 
-var data2 = {
-  list: null,
-  status:client.status
+var data = {
+  messages: null,
+  jokes: null,
+  filterMessages: null,
+  guidJokes: null,
+  maxMessagesCount: 4,
+  maxFilteredMessagesCount: 4,
+  maxJokesCount: 4,
+  maxGuidJokesCount: 4,
+  client: client
 };
 
-var syncList;
+var methods = {
+  createNew: () => {
+    Vue.http.get("/crud/create");
+  },
+  createFiveNew: () => {
+    Vue.http.get("/crud/createFive");
+  },
+  changeRandom: () => {
+    Vue.http.get("/crud/changeRandom");
+  },
+  deleteAll: () => {
+    Vue.http.get("/crud/deleteAll");
+  },
+  deleteRandom: () => {
+    Vue.http.get("/crud/deleteRandom");
+  }
+}
 
+var computed = {
+  cMessages: () => {
+    var orderedArray: any[] = _.orderBy(data.messages, 'id', 'desc');
+    if (data.maxMessagesCount) {
+      return orderedArray.slice(0, data.maxMessagesCount);
+    }
+    else {
+      return orderedArray;
+    }
+  },
+  cJokes: () => {
+    var orderedArray: any[] = _.orderBy(data.jokes, 'id', 'desc');
+    if (data.maxJokesCount) {
+      return orderedArray.slice(0, data.maxJokesCount);
+    }
+    else {
+      return orderedArray;
+    }
+  },
+  cFilterMessages: () => {
+    var orderedArray: any[] = _.orderBy(data.filterMessages, 'id', 'desc');
+    if (data.maxFilteredMessagesCount) {
+      return orderedArray.slice(0, data.maxFilteredMessagesCount);
+    }
+    else {
+      return orderedArray;
+    }
+  },
+  cGuidJokes: () => {
+    var orderedArray: any[] = _.orderBy(data.guidJokes, 'id', 'desc');
+    if (data.maxGuidJokesCount) {
+      return orderedArray.slice(0, data.maxGuidJokesCount);
+    }
+    else {
+      return orderedArray;
+    }
+  }
+}
 
 client.syncWith("/subscribe/SubscribeToAllMessages")
   .then(x => {
-    data2.list = x;
-    syncList = x;
+    data.messages = x;
   });
 
-
-
-
-declare var Vue: any;
-//declare var MyPlugin: any;
-
-//var data = {
-//  list: null
-//};
-
-//Vue.http.get("/subscribe/SubscribeToAllMessages").then(function (response) {
-//  data.list = response.data;
-//}, function (error) {
-//  console.log(error.statusText);
-//});
-
-//new Vue({
-//  el: '#app',
-//  data: data
-//});
+client.syncWith("/subscribe/SubscribeToAllJokes")
+  .then(x => {
+    data.jokes = x;
+  })
+client.syncWith("/subscribe/SubscribeToOddIdMessages")
+  .then(x => {
+    data.filterMessages = x;
+  })
+client.syncWith("/subscribe/SubscribeToJokesWithGuidAnswer")
+  .then(x => {
+    data.guidJokes = x;
+  })
 
 new Vue({
   el: '#app2',
-  data: data2
+  data: data,
+  methods: methods,
+  computed:computed
 });
