536d8e0347f98143679420f94d0110dee0a8b7fc
Dustin <dustout@gmail.com>
2019-04-27 11:23:16 -0600

    Removed unused IDField on ChangedObject type

--- EntitySignal.Server/Models/ChangedObject.cs
+++ EntitySignal.Server/Models/ChangedObject.cs
@@ -6,7 +6,6 @@ namespace EntitySignal.Models
   public class ChangedObject
   {
     public Type Type { get; set; }
-    public string IdField { get; set; }
     public object Object { get; set; }
     public EntityState State { get; set; }
     public string Url { get; set; }
